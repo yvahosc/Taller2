2cd915187ba84c6b7b68f0f1beb2ce7990268973
yvahosc <yvahosc@unal.edu.co>
2022-02-07 12:57:42 -0500

    Creación de la interface nave espacial
    Creación de la interface nave espacial con sus metodos recarga de combustible y encendido de la nave espacial.

---
+++ src/Interfaces/ISpaceShip.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+/**
+ *
+ * @author Yeisson Vahos Cortes
+ */
+public interface ISpaceShip {
+    
+    
+    public String reFuelSpaceShip();
+    public String turnOnSpaceShip ();
+}
